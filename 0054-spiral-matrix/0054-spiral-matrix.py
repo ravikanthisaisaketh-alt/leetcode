95baea2a6218719d8ce5dbe9b7124e0728d28fdf
ravikanthisaisaketh-alt <ravikanthi.saisaketh@gmail.com>
2026-04-06 21:08:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,30 +1,31 @@
-class Solution:
+class Solution(object):
     def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        result = []
         top = 0
-        bottom = len(matrix) - 1
+        bottom = len(matrix)-1
         left = 0
-        right = len(matrix[0]) - 1
-        spiral = []
+        right = len(matrix[0])-1
+        while top<=bottom and left<=right:
+            for i in range(left, right+1):
+                result.append(matrix[top][i])
+            top+=1
 
-        while top <= bottom and left <= right:
-            for i in range(left, right + 1):
-                spiral.append(matrix[top][i])
-            top += 1
+            for j in range(top ,bottom+1):
+                result.append(matrix[j][right])
+            right-=1
 
-            for j in range(top, bottom + 1):
-                spiral.append(matrix[j][right])
-            right -= 1
+            if top<=bottom:
+                for i in range(right,left-1,-1):
+                    result.append(matrix[bottom][i])     
+                bottom-=1   
+            if left<=right:
+                for j in range(bottom,top-1,-1):
+                    result.append(matrix[j][left])
+                left+=1
+        return result
 
-            if top <= bottom:
-                for k in range(right, left - 1, -1):
-                    spiral.append(matrix[bottom][k])
-                bottom -= 1
-
-            if left <= right:
-                for l in range(bottom, top - 1, -1):
-                    spiral.append(matrix[l][left])
-                left += 1
-
-        return spiral
-        
         
